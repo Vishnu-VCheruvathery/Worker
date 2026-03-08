421a3b99fecb61b2afd46ca29454c85ccba0b1f0
Vishnu-VCheruvathery <c.vishnu.vasudevan@gmail.com>
2026-03-08 12:57:38 +0530

    added port

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,21 @@ import "dotenv/config";
 import './workers/uploadWorker'
 import mongoose from "mongoose";
 import { initChroma } from "./db/chromaCollection";
+import http from "http";
+const PORT = process.env.PORT ||  10000
 
 const start = async() => {
     await initChroma()
     console.log("Chroma started")
 }
+
+
+http.createServer((req, res) => {
+    res.writeHead(200);
+    res.end("Worker alive")
+}).listen(PORT, () => {
+    console.log("Health server listening on", PORT)
+})
 
 start()
 
